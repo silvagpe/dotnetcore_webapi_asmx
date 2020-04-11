ec59068a9498e95a13085ab56490bd1cc3636801
silvagpe <silvagpe@gmail.com>
2020-04-11 10:48:30 -0300

    teste

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,7 @@ namespace dotnetcoreWebApi
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        //Teste
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
